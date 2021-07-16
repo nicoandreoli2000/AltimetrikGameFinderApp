9c2ccc0bf8b383fd73b04ffb0aa01b564af9c3bf
nicoandreoli2000 <nicolasandreoli2000@hotmail.com>
2021-07-16 15:56:47 -0300

    Sueggest search bar ok, but bug of fonts...

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,15 @@
 //HTML references
 const logoutButton = document.querySelector('.header__logout-button');
+const searchInput = document.querySelector('.header__search-input');
+const searchBox = document.querySelector('.header__search');
 
 //Events
 logoutButton.addEventListener('click', () => {
     window.location.href = 'login.html';
+});
+searchInput.addEventListener('input', () => {
+    searchBox.classList.add('searchSuggestion');
+});
+searchInput.addEventListener('blur', () => {
+    searchBox.classList.remove('searchSuggestion');
 });
