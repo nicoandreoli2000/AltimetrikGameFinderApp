95d4003229c0f26b22e044d70f3e27f1c3d79c47
nicoandreoli2000 <nicolasandreoli2000@hotmail.com>
2021-07-19 14:22:12 -0300

    shadow, grid, margin bottom, trying with radiobuttons now in js

--- src/components/main.js
+++ src/components/main.js
@@ -2,14 +2,25 @@
 const logoutButton = document.querySelector('.header__logout-button');
 const searchInput = document.querySelector('.header__search-input');
 const searchBox = document.querySelector('.header__search');
+const radioDisplays = document.querySelectorAll('.main__radio');
+const listCards = document.querySelector('.main__ul-grid');
 
-//Events
+//Events for search bar
 logoutButton.addEventListener('click', () => {
     window.location.href = 'login.html';
 });
 searchInput.addEventListener('input', () => {
     searchBox.classList.add('searchSuggestion');
 });
+searchInput.addEventListener('click', () => {
+    searchBox.classList.add('searchSuggestion');
+});
 searchInput.addEventListener('blur', () => {
     searchBox.classList.remove('searchSuggestion');
 });
+
+
+//Events for radio displays
+radioDisplays.addEventListener('click', () => {
+    listCards.classList.toggle('main__ul-grid');
+})
