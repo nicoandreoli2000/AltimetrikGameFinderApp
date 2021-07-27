ac12180921835fd894b37544008f1447c620c9d6
nicoandreoli2000 <nicolasandreoli2000@hotmail.com>
2021-07-27 18:30:07 -0300

    Still gotta finish mobile main

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,16 @@
 
 
 //HTML references
-const logoutButton = document.querySelector('.header__logout-button');
+const logoutButton1 = document.querySelector('.header__logout-button');
+const logoutButton2 = document.querySelector('.menu__logout-button');
 const searchInput = document.querySelector('.header__search-input');
 const searchBox = document.querySelector('.header__search');
 const listCards = document.querySelector('.main__ul-grid');
 const customMenu = document.querySelector('.menu');
 const menuButton = document.querySelector('.header__menu-button');
-const wrapper = document.querySelector('.wrapper');
+const searchButton = document.querySelector('.header__search-button');
+const searchMobile = document.querySelector('.header__search--mobile');
+const header = document.querySelector('.header');
 
 //Radio button displays
 const addGrid = () => {
@@ -25,22 +28,67 @@ const addFlex = () => {
 }
 
 //Logout event
-logoutButton.addEventListener('click', () => {
+logoutButton1.addEventListener('click', () => {
+    window.location.href = 'login.html';
+});
+logoutButton2.addEventListener('click', () => {
     window.location.href = 'login.html';
 });
 
 //Menu pop up
-// wrapper.addEventListener('click', () => {
-//     if (customMenu.classList.contains('tabletMenu')) {
-//         customMenu.classList.remove('tabletMenu');
-//     }
-// });
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener('click', (event) => {
+
     customMenu.classList.add('tabletMenu');
+    // console.log('agrego menu');
+    // setTimeout(() => {
+    //     listen();
+    // }, 100);
 });
+
 customMenu.addEventListener('click', () => {
     customMenu.classList.remove('tabletMenu');
-})
+});
+
+// const listen = () => {
+//     document.addEventListener('click', (event) => {
+
+//         if (customMenu.classList.contains('tabletMenu')) {
+
+//             const path = event.path;
+//             const actual = document.querySelector('.menu.tabletMenu');
+//             let has = true;
+
+//             path.forEach(element => {
+//                 if (actual === element) {
+//                     has = false;
+//                 }
+
+//             });
+
+//             if (has) {
+//                 customMenu.classList.remove('tabletMenu');
+//                 document.removeEventListener();
+//                 console.log('remuevo menu');
+//             }
+//         }
+//     })
+// };
+
+
+//Search bar pop up
+searchButton.addEventListener('click', () => {
+
+    searchMobile.classList.toggle('flex-horizontal');
+
+    if (searchMobile.classList.contains('flex-horizontal')) {
+        header.style.height = '166px';
+
+    } else {
+        header.style.height = '';
+    }
+
+});
+
 
 //Events for search bar
 searchInput.addEventListener('focus', () => {
@@ -50,6 +98,4 @@ searchInput.addEventListener('focus', () => {
 searchInput.addEventListener('blur', () => {
     searchBox.classList.remove('isFocused');
     searchBox.classList.remove('searchSuggestion');
-    // setTimeout(() => {
-    // }, 3000);
 });
